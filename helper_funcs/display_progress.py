2cec56c4efb344c4ae410e457f0137d9916ea465
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2024-09-07 21:21:52 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -30,9 +30,9 @@ async def progress_for_pyrogram(
     )
     
         if round(diff % 10.00) == 0 or current == total:
-        # if round(current / total * 100, 0) % 5 == 0:
-        percentage = current * 100 / total
-        speed = current / diff
+    # if round(current / total * 100, 0) % 5 == 0:
+    percentage = current * 100 / total
+    speed = current / diff
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion

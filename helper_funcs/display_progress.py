5a0a3541693a77d1d121ecfa9a1435e16047f0c1
chai35217793 <149489960+chai35217793@users.noreply.github.com>
2024-09-07 21:37:07 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -2,7 +2,7 @@ import logging
 import math
 import os
 import time
-
+from pyrogram import InlineKeyboardButton, InlineKeyboardMarkup
 
 async def progress_for_pyrogram(
     current,
@@ -28,11 +28,10 @@ async def progress_for_pyrogram(
             ]
         ]
     )
-    
-        if round(diff % 10.00) == 0 or current == total:
-    # if round(current / total * 100, 0) % 5 == 0:
-    percentage = current * 100 / total
-    speed = current / diff
+
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
         elapsed_time = round(diff) * 1000
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
@@ -43,13 +42,13 @@ async def progress_for_pyrogram(
         progress = "<b>╭────── ⌊__UploadinG: 〘{2}%〙 📤__⌉</b>\n│ \n<b>├〖{0}{1}〗</b>\n".format(
             ''.join(["▣" for i in range(math.floor(percentage / 10))]),
             ''.join(["□" for i in range(10 - math.floor(percentage / 10))]),
-            round(percentage, 2))
+            round(percentage, 2)
+        )
 
-        tmp = progress +"│" + "\n**├Total 📀:**   〘<code>{1}</code>〙\n**├Done ✓ :** 〘<code>{0}</code>〙\n**├Speed** 🚀 :  〘<code>{2}/s</code>〙\n**├ETA** ⏳ :  〘<code>{3}</code>〙\n**│**\n**╰── ⌊ @All_Movie_Rockers ⌉**".format(
+        tmp = progress + "│" + "\n**├Total 📀:**   〘<code>{1}</code>〙\n**├Done ✓ :** 〘<code>{0}</code>〙\n**├Speed** 🚀 :  〘<code>{2}/s</code>〙\n**├ETA** ⏳ :  〘<code>{3}</code>〙\n**│**\n**╰── ⌊ @All_Movie_Rockers ⌉**".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
-            # elapsed_time if elapsed_time != '' else "0 s",
             estimated_total_time if estimated_total_time != '' else "0 s"
         )
         try:
@@ -62,10 +61,7 @@ async def progress_for_pyrogram(
         except:
             pass
 
-
 def humanbytes(size):
-    # https://stackoverflow.com/a/49361727/4723940
-    # 2**10 = 1024
     if not size:
         return ""
     power = 2**10
@@ -75,7 +71,6 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
-
 
 def TimeFormatter(milliseconds: int) -> str:
     seconds, milliseconds = divmod(int(milliseconds), 1000)
